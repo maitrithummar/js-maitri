4f7577403da46e4ba7e0c87334811b31f7a37371
maitrithummar <maitrithummar24@gmail.com>
2026-07-06 09:15:01 +0000

    variable done

---
+++ basic/variable.js
@@ -0,0 +1,15 @@
+const accountId =123456
+let accountEmail = "maitri@gmail.com"
+var accountpaasword ="12345"
+accountcity ="surat"
+let accountstate;
+
+//accountId = 2
+
+accountEmail ="test@gmail.com"
+accountpaasword ="212121"
+accountcity = "ahemdabad"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountpaasword,accountcity,accountstate]);

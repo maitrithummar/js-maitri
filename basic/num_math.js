123156fc5d45f9577af94aa049faf18da1e279ed
maitrithummar <maitrithummar24@gmail.com>
2026-07-07 06:27:13 +0000

    num and math done

---
+++ basic/num_math.js
@@ -0,0 +1,51 @@
+const score= 500;
+
+// console.log(score.toString().length);
+// console.log(score.toFixed(2));
+
+const othernumber = 112.89
+//console.log(othernumber.toPrecision(3));
+
+const main =1000000;
+//console.log(main.toLocaleString('en-IN'));
+
+//math
+
+// console.log(Math);
+// console.log(Math.abs(-98));
+// console.log(Math.round(5.9));
+// console.log(Math.ceil(5.2));//moti value hoy toy pn first j ley
+// console.log(Math.floor(5.9));//nani value hoy toy pn second j ley
+// console.log(Math.min(5,8,2,3,9));
+// console.log(Math.max(5,10,15,89));
+
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+// console.log(Math.floor(Math.random()* (min -max +1 ) +min));
+
+
+//date an time 
+
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toLocaleString());
+
+// let newdate = new Date(2004 ,3 ,02);
+let newdate =new Date("2004-04-02")
+// console.log(newdate.toLocaleString());
+
+//Time stemp
+
+let newtimestemp = Date.now()
+
+// console.log(newtimestemp.toString());
+// console.log(newdate.getTime());
+
+//second mai value 
+
+// console.log(Math.floor(Date.now()/1000));
+
